4a73ae8def24ba6096ea4a2c214aed3e557e217e
Gaurang <gaurang.ma@gmail.com>
2017-08-09 11:37:31 +0530

    + adding model

---
+++ model/data.js
@@ -0,0 +1,46 @@
+module.exports = {
+"Employees" : [
+{
+"id":"1001",
+"userId":"rirani",
+"jobTitleName":"Developer",
+"firstName":"Romin",
+"lastName":"Irani",
+"preferredFullName":"Romin Irani",
+"employeeCode":"E1",
+"region":"CA",
+"phoneNumber":"408-1234567",
+"emailAddress":"romin.k.irani@gmail.com",
+"doj":"1427241600000",
+"salary":"25000"
+},
+{
+"id":"1002",
+"userId":"nirani",
+"jobTitleName":"Developer",
+"firstName":"Neil",
+"lastName":"Irani",
+"preferredFullName":"Neil Irani",
+"employeeCode":"E2",
+"region":"CA",
+"phoneNumber":"408-1111111",
+"emailAddress":"neilrirani@gmail.com",
+"doj":"1272153600000",
+"salary":"1050000"
+},
+{
+"id":"1003",
+"userId":"thanks",
+"jobTitleName":"Program Directory",
+"firstName":"Tom",
+"lastName":"Hanks",
+"preferredFullName":"Tom Hanks",
+"employeeCode":"E3",
+"region":"CA",
+"phoneNumber":"408-2222222",
+"emailAddress":"tomhanks@gmail.com",
+"doj":"590889600000",
+"salary":"235000"
+}
+]
+}
